1427a40a6fb4e40f65eb0366131bed49cf30a13f
keklikabdullah07 <keklikabdullah07@gmail.com>
2022-07-02 16:29:26 +0300

    version1

---
+++ src/containers/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Header = () => {
+    return (
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>Partridge</h2>
+            </div>
+            <button>
+                <i class=" big shopping cart icon"></i>
+            </button>
+
+        </div>
+    )
+};
+export default Header;
